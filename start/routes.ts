00aa5fb3d4186e876b106e12c44257e371b9ce41
Jumvi <judahmvi@gmail.com>
2025-03-19 17:10:16 +0100

    refactor: update routing logic by removing authentication middleware from specific routes

--- start/routes.ts
+++ start/routes.ts
@@ -9,6 +9,7 @@
 
 import router from '@adonisjs/core/services/router'
 import { middleware } from './kernel.js'
+import UsersController from '#controllers/users_controller'
 
 const urls = ()=>import('#controllers/urls_controller')
 const auth = ()=>import('#controllers/users_controller')
@@ -22,12 +23,8 @@ router.on('/pages/login').render('pages/login').as('login')
 
 
 
-router.post('/url',[urls,'generate']).use(middleware.auth())
-router.get('pages/goToUrl',[urls,'showUrls']).as('goToUrl').use(middleware.auth())
+router.post('/url',[urls,'generate'])
+router.get('pages/goToUrl',[urls,'showUrls']).as('goToUrl')
 
-router.post('/singIn',[auth,'singIn']).as('singIN')
-router.post('/login',[auth,'login'])
-
-router.post('/urls/:id',[urls,'deleteUrl']).as('deleteUrl').use(middleware.auth())
-router.post('/urls/update/:id', [urls, 'updateUrl']).as('updateUrl').use(middleware.auth())
-router.post('/logout', [auth, 'logout']).as('logout').use(middleware.auth())
+// router.post('/singIn',[auth,'singIn']).as('singIN')
+// router.post('/login',[auth,'login'])
