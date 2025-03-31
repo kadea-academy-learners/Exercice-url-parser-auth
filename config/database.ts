39779b2f3cbb2ebaa28999f50a5e13ac4fb1c670
Jumvi <judahmvi@gmail.com>
2025-03-31 09:38:48 +0100

    fix: update database configuration to support different environments

--- config/database.ts
+++ config/database.ts
@@ -8,14 +8,18 @@ const dbConfig = defineConfig({
   connections: {
     postgres: {
       client: 'pg',
-      connection: {
-        connectionString: env.get('DATABASE_URL'),
-        ssl: { rejectUnauthorized: false }
-      },
-      migrations: {
-        naturalSort: true,
-        paths: ['database/migrations']
-      }
+      connection: env.get('NODE_ENV') === 'production'
+        ? {
+            connectionString: env.get('DATABASE_PUBLIC_URL'),
+            ssl: { rejectUnauthorized: false }
+          }
+        : {
+            host: env.get('DB_HOST'),
+            port: env.get('DB_PORT'),
+            user: env.get('DB_USER'),
+            password: env.get('DB_PASSWORD'),
+            database: env.get('DB_DATABASE')
+          }
     }
   }
 })
